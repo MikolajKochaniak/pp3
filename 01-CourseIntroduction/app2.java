d18846765bb1076e6f91351c7d5cb2031db3991d
Mikołaj Kochaniak <114917755+MikolajKochaniak@users.noreply.github.com>
2023-10-16 20:26:33 +0200

    Add files via upload

---
+++ 01-CourseIntroduction/app2.java
@@ -0,0 +1,27 @@
+public class app2 {
+    public static void main(String[] args) throws Exception {
+
+        Student Miki = new Student();
+        Miki.name = "Mikolaj";
+        Miki.surname = "Kochaniak";
+        Miki.age = 19;
+        Miki.student_id = 12343242536543654L;
+        Miki.first_year = true;
+        Miki.field_of_study = "Informatyka Stosowana";
+        Miki.name_of_university = "UEK";
+        System.out.println(Miki.name + " " + Miki.surname + " " + Miki.age + " " + Miki.student_id + " " + Miki.first_year + " " + Miki.field_of_study + " " + Miki.name_of_university);
+
+        
+    }
+}
+class Student
+{
+    String name;
+    String surname;
+    int age;
+    Long student_id;
+    Boolean first_year;
+    String field_of_study;
+    String name_of_university;
+
+}
